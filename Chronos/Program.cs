1922843250d14bb6994302c9dc48716d6f03b860
Batmetista <batista.as.pedro@gmail.com>
2024-03-22 20:07:03 +0000

    Add prints

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,12 +10,19 @@ namespace Chronos
         {
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+
             crono1.Start();
             Thread.Sleep(600);
             crono2.Start();
             Thread.Sleep(200);
             crono1.Stop();
             crono2.Stop();
+
+            float elapsedSeconds1 = (float)crono1.ElapsedMilliseconds/10000;
+            float elapsedSeconds2 = (float)crono2.ElapsedMilliseconds/1000;
+            
+            Console.WriteLine(elapsedSeconds1.ToString("0.000"));
+            Console.WriteLine(elapsedSeconds2.ToString("0.000"));
         }
     }
 }
