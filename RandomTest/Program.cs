74b71e60b37792d094f78062f9079b57569e2a7e
Batmetista <batista.as.pedro@gmail.com>
2024-03-22 20:18:07 +0000

    Add for

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -8,6 +8,11 @@ namespace RandomTest
         {
             Random r1 = new Random(22);
             Random r2 = new Random(22);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r1.Next());
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r2.Next());
         }
     }
 }
