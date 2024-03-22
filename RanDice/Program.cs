0f62e2e2f1ef8fd7925e8dcc239dab7e189dab27
Batmetista <batista.as.pedro@gmail.com>
2024-03-22 20:22:39 +0000

    Add variables

---
+++ RanDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+            Random random = new Random(s);
+        }
+    }
+}
