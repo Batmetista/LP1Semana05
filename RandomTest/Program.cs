a3f41cb6b2d2ec78c13ac85f2b388a2dfd767f60
Batmetista <batista.as.pedro@gmail.com>
2024-03-22 20:13:10 +0000

    Add random variables

---
+++ RandomTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RandomTest
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r1 = new Random(22);
+            Random r2 = new Random(22);
+        }
+    }
+}
